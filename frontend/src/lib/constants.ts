1cd66d364ab0af5b81d5a42093edf9a123247638
Joeloffbeat <joeloffbeat@gmail.com>
2026-02-07 19:58:19 +0530

    chore: update contract addresses after redeployment
    New dual-coin contract deployed with submit_sell_order/submit_buy_order:
    - Package: 0xca85bfc10d129d5e4f7bcabeecd6a332568e75b23fa9a929c0b83fc8c7aee2b2
    - Pool: 0x3f47d9eaa0e6a1a159bbdd1fdc8a8bcc46252c51ed7e0494d1e10341c2ba9e58
    - MatcherCap: 0xc6bd007eeebc06c276c7cf2d29177688ee40c9904aad662eb3250b2cf5ff9edd

--- frontend/src/lib/constants.ts
+++ frontend/src/lib/constants.ts
@@ -1,7 +1,7 @@
 // Deployed contract addresses (Sui testnet)
-export const DARK_POOL_PACKAGE = '0x381920f137dcbc01865fddb24d48b147d9caaa34b6c9a431e6081bbe0e31d84f';
-export const DARK_POOL_OBJECT = '0x97fd88d921bb0f70f93a03ff63d89a31aa08227cea0847413b06c2d5cba04344';
+export const DARK_POOL_PACKAGE = '0xca85bfc10d129d5e4f7bcabeecd6a332568e75b23fa9a929c0b83fc8c7aee2b2';
+export const DARK_POOL_OBJECT = '0x3f47d9eaa0e6a1a159bbdd1fdc8a8bcc46252c51ed7e0494d1e10341c2ba9e58';
 export const SEAL_PACKAGE_ID = '0x8afa5d31dbaa0a8fb07082692940ca3d56b5e856c5126cb5a3693f0a4de63b82';
 export const SEAL_ALLOWLIST_ID = '0xba6fda6cbedd1938debcec8883b07f1c7d2d1b9d744fe57c98c48e75cf05acf8';
 export const DBUSDC_TYPE = '0xf7152c05930480cd740d7311b5b8b45c6f488e3a53a11c3f74a6fac36a52e0d7::DBUSDC::DBUSDC';
-export const BACKEND_URL = 'http://localhost:3001';
+export const BACKEND_URL = 'http://localhost:3006';
